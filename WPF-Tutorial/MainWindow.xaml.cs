52c8bea1584cc723cbf058927a5a836b3ddb3411
psyGamer <psygamer21@gmail.com>
2022-05-21 18:42:06 +0200

    Initial commit

---
+++ WPF-Tutorial/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Tutorial
+{
+	/// <summary>
+	/// Interaktionslogik für MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void SubmitForm(object sender, RoutedEventArgs e)
+		{
+			string firstName = firstNameBox.Text;
+			string lastName = lastNameBox.Text;
+
+			firstNameBox.Clear();
+			lastNameBox.Clear();
+
+			firstNameBox.Visibility = Visibility.Hidden;
+			firstNameLabel.Visibility = Visibility.Hidden;
+			lastNameBox.Visibility = Visibility.Hidden;
+			lastNameLabel.Visibility = Visibility.Hidden;
+			submitButton.Visibility = Visibility.Hidden;
+			exitButton.Visibility = Visibility.Visible;
+
+			successMessage.Content = $"Vielen Dank für Ihren einkauf, {firstName} {lastName}.";
+		}
+
+		private void ExitProgram(object sender, RoutedEventArgs e) {
+			System.Windows.Application.Current.Shutdown();
+		}
+	}
+}
